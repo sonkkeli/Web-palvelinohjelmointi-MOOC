3ab465c681b4aa7bf60def2b8c2373b41c2f12b8
Sonja <46299415+sonkkeli@users.noreply.github.com>
2019-03-18 18:57:43 +0200

    Add files via upload

---
+++ osa02-Osa02_11.HelloIndividualPages/src/main/java/helloindividualpages/HelloIndividualPagesController.java
@@ -0,0 +1,41 @@
+package helloindividualpages;
+
+import java.util.Map;
+import java.util.TreeMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloIndividualPagesController {
+
+    private Map<String, Item> items;
+
+    public HelloIndividualPagesController() {
+        this.items = new TreeMap<>();
+        Item item = new Item("Wizard hat", "pointy");
+        this.items.put(item.getIdentifier(), item);
+    }
+    
+    @GetMapping("/{item}")
+    public String getOne(Model model, @PathVariable String item){
+        model.addAttribute("item", this.items.get(item));
+        return "single";
+    }
+
+    @PostMapping("/")
+    public String post(@RequestParam String name, @RequestParam String type) {
+        Item item = new Item(name, type);
+        this.items.put(item.getIdentifier(), item);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("items", this.items.values());
+        return "index";
+    }
+}
