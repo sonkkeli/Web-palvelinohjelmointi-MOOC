6ebc70650f4f92fbb03dc9b78f4d45bf7c6b6888
Sonja <46299415+sonkkeli@users.noreply.github.com>
2019-03-23 16:00:04 +0200

    Add files via upload

---
+++ osa03-Osa03_02.SimpleBanking/src/main/java/simplebanking/BankingController.java
@@ -0,0 +1,50 @@
+package simplebanking;
+
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BankingController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @GetMapping("/")
+    public String list(Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        return "index";
+    }
+    @Transactional
+    @PostMapping("/")
+    public String add(@RequestParam String name, @RequestParam String iban) {
+        if (name.trim().isEmpty() || iban.trim().isEmpty()) {
+            return "redirect:/";
+        }
+
+        Client client = clientRepository.findByName(name);
+        if (client == null) {
+            client = new Client();
+            client.setName(name);
+            client = clientRepository.save(client);
+        }
+        Account account = accountRepository.findByIban(iban);
+        if (account == null) {
+            account = new Account();
+            account.setIban(iban);
+            account.setBalance(100);
+            account = accountRepository.save(account);
+        }
+        // DO SOMETHING HERE
+        client.getAccounts().add(account);
+        account.setClient(client);
+        
+        return "redirect:/";
+    }
+}
