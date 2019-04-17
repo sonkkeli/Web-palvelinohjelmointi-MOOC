241b71666fec04430194469208abfab71f8794bb
sonkkeli <46299415+sonkkeli@users.noreply.github.com>
2019-04-17 15:16:17 +0300

    Add files via upload

---
+++ osa06-Osa06_06.Registration/src/main/java/registration/Registration.java
@@ -0,0 +1,29 @@
+package registration;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Registration extends AbstractPersistable<Long> {
+
+    
+    @NotEmpty
+    @Size(min = 4, max = 50)
+    private String name;
+    
+    @NotEmpty
+    @Size(min = 4, max = 50)
+    private String address;
+    
+    @NotEmpty
+    @Email
+    private String email;
+
+}
